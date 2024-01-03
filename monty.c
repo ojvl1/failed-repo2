9c74f1aa7cd0bcca72b295b6a0a8355da27c8d65
osvaldo antompietri <antompietriosvaldo@gmail.com>
2024-01-03 16:35:36 -0400

    add white spaces function and clean up function

--- monty.c
+++ monty.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "monty.h"
+
 
 void clean_up(char *line, stack_t **stack, FILE *file)
 {
@@ -8,6 +8,31 @@ void clean_up(char *line, stack_t **stack, FILE *file)
 	free_dlist(stack);
 	free(stack);
 	fclose(file);
+}
+
+/**
+ * white_spaces - checks if args is null, has only white spaces
+ * @args: array from get_tokens, from main
+ * @str_line: string from main, from stdin
+ * Return: 0 if args was null(white spaces only)
+ */
+
+int white_spaces(char **args, unsigned int line_number)
+{
+	if (args[0] == NULL) /* white spaces only */
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, args[0]);
+		free(args);
+		return (0);
+	}
+
+	return (-1);
+}
+
+void arg_validation(void)
+{
+	printf("USAGE: monty bytecodes/001.m\n");
+	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv)
@@ -24,10 +49,7 @@ int main(int argc, char **argv)
 
 
 	if (argc != 2 || argv[1] == NULL)
-	{
-		printf("USAGE: monty bytecodes/001.m\n");
-		exit(EXIT_FAILURE);
-	}
+		arg_validation();
 
 	FILE *file;
 	stack = malloc(sizeof(stack_t *));
@@ -45,6 +67,11 @@ int main(int argc, char **argv)
 		line_number++;
 		line[strlen(line) - 1] = '\0'; /* remove \n from the end of line */
 		args = get_tokens(line); /* makes tokens and args array with mallocs */
+		if (white_spaces(args, line_number) == 0)
+		{
+			clean_up(line, stack, file);
+			exit(EXIT_FAILURE);
+		}
 		for (i = 0; instructions[i].f != NULL; i++) /*Still have functions to call*/
 		{
 			if (strcmp(instructions[i].opcode, args[0]) == 0) /* Matched a case */
